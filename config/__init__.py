bed364f6f88e205d7f31207fc4206c046563c23d
rolandchao <roland.chao@91app.com>
2025-12-01 00:51:52 +0800

    Revert "add homework"
    This reverts commit 246827e9d61aa21c442329285a8f923b8d63785d.

--- config/__init__.py
+++
@@ -1,3 +0,0 @@
-from .browser_config import get_chrome_options
-
-__all__ = ['get_chrome_options']
--- page_objects/twitch/__init__.py
+++
@@ -1,8 +0,0 @@
-from .home_page.action import HomePageAction
-from .search_page.action import SearchPageAction
-
-
-class TwitchPages:
-    def __init__(self, driver):
-        self.home = HomePageAction(driver)
-        self.search = SearchPageAction(driver)
